6c3e78c395efa6ce3eac60627532f4e4e05d0a53
Jacob Dominic <>
2021-05-03 13:49:04 +0000

    indent changes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.getElementById('report').addEventListener('click',reportResults)//adds report button as an event listener
+document.getElementById('report').addEventListener('click', reportResults)// adds report button as an event listener
 
 function reportResults () {
   alert(document.getElementById('input').value)
-}//this function tells my webpage to report results typed into the search engine machine
+}// this function tells my webpage to report results typed into the search engine machine
 
-function changeBackground(color) {
-   document.body.style.background = color
+function changeBackground (color) {
+  document.body.style.background = color
 }// function that changes the background color of my code to the color specified in my addEventListener
 
-window.addEventListener("load",function() { changeBackground('blue') });// Event listener that says to load the function to change my background color to blue
+window.addEventListener('load', function () { changeBackground('blue') })// Event listener that says to load the function to change my background color to blue
